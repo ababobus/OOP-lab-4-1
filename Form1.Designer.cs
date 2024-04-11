0e738fa32257ce73c4e39fdbe0af1e54acd03241
Азалия Баймуратова <ozalea@yandex.ru>
2024-04-11 19:53:22 +0500

    done 1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,31 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.Figures = new System.Windows.Forms.PictureBox();
+            this.CtrlCheckBox = new System.Windows.Forms.CheckBox();
+            this.OverlayCheckBox = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Figures)).BeginInit();
             this.SuspendLayout();
             // 
-            // checkBox1
+            // Figures
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(114, 50);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(113, 24);
-            this.checkBox1.TabIndex = 0;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.Figures.Location = new System.Drawing.Point(1, 135);
+            this.Figures.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Figures.Name = "Figures";
+            this.Figures.Size = new System.Drawing.Size(1102, 681);
+            this.Figures.TabIndex = 1;
+            this.Figures.TabStop = false;
+            this.Figures.Paint += new System.Windows.Forms.PaintEventHandler(this.Figures_Paint);
+            this.Figures.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Figures_MouseClick);
+            // 
+            // CtrlCheckBox
+            // 
+            this.CtrlCheckBox.AutoSize = true;
+            this.CtrlCheckBox.Location = new System.Drawing.Point(59, 28);
+            this.CtrlCheckBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.CtrlCheckBox.Name = "CtrlCheckBox";
+            this.CtrlCheckBox.Size = new System.Drawing.Size(76, 24);
+            this.CtrlCheckBox.TabIndex = 2;
+            this.CtrlCheckBox.Text = "CTRL";
+            this.CtrlCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // OverlayCheckBox
+            // 
+            this.OverlayCheckBox.AutoSize = true;
+            this.OverlayCheckBox.Location = new System.Drawing.Point(59, 76);
+            this.OverlayCheckBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.OverlayCheckBox.Name = "OverlayCheckBox";
+            this.OverlayCheckBox.Size = new System.Drawing.Size(112, 24);
+            this.OverlayCheckBox.TabIndex = 3;
+            this.OverlayCheckBox.Text = "OVERLAY";
+            this.OverlayCheckBox.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.checkBox1);
+            this.ClientSize = new System.Drawing.Size(1105, 816);
+            this.Controls.Add(this.OverlayCheckBox);
+            this.Controls.Add(this.CtrlCheckBox);
+            this.Controls.Add(this.Figures);
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Figures)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -60,7 +90,9 @@
 
         #endregion
 
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.PictureBox Figures;
+        private System.Windows.Forms.CheckBox CtrlCheckBox;
+        private System.Windows.Forms.CheckBox OverlayCheckBox;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -19,15 +19,15 @@ namespace WindowsFormsApp1
             InitializeComponent();
         }
 
-        static Pen PenCircleSelect = new Pen(Brushes.HotPink);
-        static Pen PenCircleNotSelect = new Pen(Brushes.Black);
+        static readonly Pen PenCircleSelect = new Pen(Brushes.HotPink);
+        static readonly Pen PenCircleNotSelect = new Pen(Brushes.Black);
         void PenCircleMake()
         {
             PenCircleSelect.Width = 2;
             PenCircleNotSelect.Width = 2;
         }
         const int radius = 30;
-
+        bool IsCtrl = false;
         class CCircle
         {
             public Rectangle crug;
@@ -35,6 +35,7 @@ namespace WindowsFormsApp1
             public CCircle(int x, int y)
             {
                 crug = new Rectangle(x, y, radius * 2, radius * 2);
+                Selected = true;
             }
 
             public void Draw(PaintEventArgs e) 
@@ -67,25 +68,74 @@ namespace WindowsFormsApp1
         List<CCircle> Circles = new List<CCircle>();
 
 
-        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        private void Figures_MouseClick(object sender, MouseEventArgs e)
         {
+            bool CircleClickFlag = false;
+            if (IsCtrl  == false|| CtrlCheckBox.Checked == false)
+                foreach (var i in Circles)
+                    i.SetSelect(false);
 
+            foreach (var i in Circles) 
+            {
+                if (i.IsClicked(e.X - radius, e.Y - radius))
+                {
+                    i.ChangeSelect();
+                    CircleClickFlag = true;
+                    if (OverlayCheckBox.Checked == false)
+                        break;
+                }
+            }
+
+            if (CircleClickFlag == false)
+            {
+                if (IsCtrl == false|| CtrlCheckBox.Checked == false)
+                    foreach (var i in Circles)
+                        i.SetSelect(false);
+                Circles.Add(new CCircle(e.X - radius, e.Y - radius));
+
+            }
+            Figures.Invalidate();
         }
 
-        private void Form1_KeyUp(object sender, KeyEventArgs e)
-        {
-
-        }
         private void Form1_Load(object sender, EventArgs e)
         {
             PenCircleMake();
         }
-
-        private void Form1_Paint(object sender, PaintEventArgs e)
+        private void Figures_Paint(object sender, PaintEventArgs e) // Событие отрисовки
         {
-
+            foreach (var i in Circles)
+                i.Draw(e);
         }
-        //add btn delete save group load
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete) 
+            {
+                for (int i = 0; i < Circles.Count(); ++i)
+                {
+                    if (Circles[i].GetSelect())
+                    {
+                        Circles.RemoveAt(i--);
+                    }
+                }
+                if (Circles.Count != 0)
+                    Circles.Last().SetSelect(true);
+
+                Figures.Invalidate();
+            }
+            else if (e.Control) // Нажатие CTRL
+            {
+                IsCtrl = true;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.ControlKey)
+            {
+                IsCtrl = false;
+            }
+        }
     }
 
 
